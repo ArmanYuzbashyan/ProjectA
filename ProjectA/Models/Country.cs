ae707aba8de0f69080c601630f252b911f156924
ArmanYuzbashyan <arman.yuzbashyan@gmail.com>
2020-09-29 03:24:25 +0400

    Model updates

--- ProjectA/Models/Country.cs
+++ ProjectA/Models/Country.cs
@@ -7,8 +7,13 @@ namespace ProjectA.Models
 {
     public class Country
     {
+        public Country()
+        {
+            NationalCompetitions = new HashSet<NationalCompetition>();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public Region Region { get; set; }
+        public ICollection<NationalCompetition> NationalCompetitions { get; set; }
     }
 }
--- ProjectA/Models/Region.cs
+++ ProjectA/Models/Region.cs
@@ -7,7 +7,12 @@ namespace ProjectA.Models
 {
     public class Region
     {
+        public Region()
+        {
+            GlobalCompetitions = new HashSet<GlobalCompetition>();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<GlobalCompetition> GlobalCompetitions { get; set; }
     }
 }
